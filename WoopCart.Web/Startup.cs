885bcc14ed45a5ef7e06f55262e16d1757bb9b64
yash desai <yashdesai1010@gmail.com>
2022-12-31 14:11:32 +0530

    Add project files.

---
+++ WoopCart.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WoopCart.Web.Startup))]
+namespace WoopCart.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
